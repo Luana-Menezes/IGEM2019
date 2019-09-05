349dc31467950d5f356b49aac6f6ef617fa11272
luana menezes <luana.menezes.lm@gmail.com>
2019-09-05 00:38:21 -0300

    Arrumado pagina interna: menu lateral scrollpy, fundo estrelado; pagina principal: video com fotos

---
+++ js/myscript.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+ 
+ // Smooth Scrolling
+ $('#scrollspy-menu a').on('click', function(event) {
+   if (this.hash !== '') {
+     event.preventDefault();
+     const hash = this.hash;
+     $('html, body').animate(
+       {scrollTop: $(hash).offset().top
+       },800,
+       function() {
+         window.location.hash = hash;
+       }
+     );
+   }
+ });
+
+});
