78267b4eefa32da25be8a0b19c5ca082cd6e8a09
luana menezes <luana.menezes.lm@gmail.com>
2019-09-06 21:49:57 -0300

    Quase tudo pronto, fundo perfeito, menu perfeito, fonts importadas, cores do boostrap modificadas, novos css e js de frameworks

--- js/myscript.js
+++ js/myscript.js
@@ -1,5 +1,24 @@
+AOS.init();
+  
+  
+  
+  //Navbar Scrolling
+(function ($) {
+  var SCROLLING_NAVBAR_OFFSET_TOP = 50;
+  $(window).on('scroll', function () {
+    var $navbar = $('.navbar');
+
+    if ($navbar.length) {
+      if ($navbar.offset().top > SCROLLING_NAVBAR_OFFSET_TOP) {
+        $('.scrolling-navbar').addClass('top-nav-collapse');
+      } else {
+        $('.scrolling-navbar').removeClass('top-nav-collapse');
+      }
+    }
+  });
+})(jQuery);
+
 $(document).ready(function(){
- 
  // Smooth Scrolling
  $('#scrollspy-menu a').on('click', function(event) {
    if (this.hash !== '') {
